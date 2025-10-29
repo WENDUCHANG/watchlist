e133f18039dbc81608173cd74eee1e6e4ed39ef8
Wangyifeng <1142487034@qq.com>
2025-10-29 16:17:17 +0800

    加入静态文件

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-name = 'Grey Li'
+name = 'Wang Yifeng'
 movies = [
     {'title': 'My Neighbor Totoro', 'year': '1988'},
     {'title': 'Dead Poets Society', 'year': '1989'},
